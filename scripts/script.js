66a6318d4340189faa8db9cb844c74cdfe2fd7e2
KikoJade <kirianweidner@mail.weber.edu>
2025-09-04 16:14:43 -0600

    Added theme settings

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+function darkMode() {
+    document.getElementById('mainPage').style="background-color: #505050; color: #FEFEFE;";
+    document.getElementById('title').style="background-color: #707070;";
+}
+
+function lightMode() {
+    document.getElementById('mainPage').style="background-color: #FEFEFE; color: #101010;";
+    document.getElementById('title').style="background-color: #DDDDDD;";
+}
+
+function defaultMode() {
+    document.getElementById('mainPage').style="background-color: cadetblue; color: #000000;";
+    document.getElementById('title').style="background-color: rgb(67, 90, 118);";
+}
